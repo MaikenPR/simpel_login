d6dfb8ee216975d14f42b5a617a4130fdf146772
MaikenPR <438552@rts-365.dk>
2018-06-19 19:49:36 +0200

    nu med fuld CRUD

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,40 @@
 const authenticate = require('../middleware/authenticate');
+const db = require('../config/database').connect();
+const Hash = require('../services/hash');
+const User = require('../services/users');
 
+// READ - hent bruger
 module.exports = function (app) {
-    app.get('/profile',  authenticate, (req, res) => {
-        res.render('profile', { 'page': { 'title': 'Profile' } });
+    app.get('/profile', authenticate, async (req, res) => { //authenticate bestemmer om man skal være logget ind eller ej for at kunne se siden
+        try{
+            const username = await User.getOne(req.session.isLoggedIn.id);
+            res.render('profile', {'page': {'title': 'Profile'}, 'content': {'username': username}})
+        } catch(error){
+            res.send(error);
+        }
+    });
+
+    //UPDATE - Rediger bruger
+    app.post('/profile/update', authenticate, async (req, res) => {
+        try{
+            await User.update(req.session.isLoggedIn.id, req.body.password)
+            res.send('Dit password er ændret');
+            
+        } catch(error){
+            res.send('Der skete en fejl');
+        }
+    });
+
+    //DELETE - Slet bruger
+    app.get('/profile/delete', authenticate, async (req, res) => {
+       try{
+            await User.deleteOne(req.session.isLoggedIn.id);
+            req.session.destroy((err) => {
+                res.send('Din bruger er slettet. Goodbye!');
+            });
+       } catch(error){
+            res.send(error);
+       }
     });
 };
 
--- services/users.js
+++ services/users.js
@@ -1,6 +1,7 @@
 const db = require('../config/database').connect();
 const Hash = require('./hash');
 
+//CREATE - opretter en user
 const User = function (username, password) {
     return new Promise(async (resolve, reject) => {
         const hash = await Hash(password);
@@ -12,11 +13,43 @@ const User = function (username, password) {
     });
 };
 
+//READ - henter/viser en bruger
+User.getOne = function (id) {
+    return new Promise((resolve, reject) => {
+        db.execute('SELECT username FROM users WHERE id = ?', [id], (error, result) => {
+            if (error) reject(error);
+            resolve(result[0].username);
+        });
+    });
+};
+
+//UPDATE - Updaterer password 
+User.update = function (id, password) {
+    return new Promise(async (resolve, reject) => {
+        const hash = await Hash(password);
+        db.execute('UPDATE users SET password = ? WHERE id = ?', [hash, id], (error) => {
+            if (error) reject(error);
+            resolve();
+        });
+    });
+};
+
+//DELETE - Sletter en bruger
+User.deleteOne = function (id) {
+    return new Promise((resolve, reject) => {
+        db.execute('DELETE FROM users WHERE id = ?', [id], (error, result) => {
+            if (error) reject(error);
+            resolve();
+        });
+    });
+};
+
+//tjekker om brugernavn og adgangskode er korrekt
 User.valid = function (username, password) {
     return new Promise((resolve, reject) => {
         db.execute('SELECT id, password FROM users WHERE username = ?', [username], async (err, result) => {
             if (err) reject(err);
-            if(result[0] === undefined) reject('invalid stuffs');
+            if (result[0] === undefined) reject('invalid stuffs');
             if (await Hash.compare(password, result[0].password)) {
                 resolve(result[0].id);
             } else {
